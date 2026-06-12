28f95ded8ac09f643943bbe8055f63d34ab478d8
AdityaGaur26 <adityagaur2609@gmail.com>
2026-06-12 18:00:39 +0530

    footer update

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ const App = () => {
       </div>
       <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - by ADITYA</p>
+    <p>Copyright © {new Date().getFullYear()} - by ADITYA</p> 
   </aside>
 </footer>
     </div>
